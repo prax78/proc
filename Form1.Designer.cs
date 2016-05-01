4974388dcd029847254281a605e22683fb681e6e
prax78 <prax78@gmail.com>
2016-05-02 00:04:41 +0530

    Added files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace proc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Server Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 99);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Size = new System.Drawing.Size(477, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Enter Server Name to Get Process";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 301);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Get Server Process and Close 0.1 Beta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace proc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void butaction()
+        {
+            if ((textBox1.Text != "") && (!textBox1.Text.Contains("Enter Server Name to Get Process")))
+            {
+                string comp = textBox1.Text.ToString();
+
+
+                try
+                {
+                    using (ManagementObjectSearcher sc = new ManagementObjectSearcher("\\\\" + comp + "\\root\\cimv2", "select * from win32_process"))
+                    {
+                        ManagementObjectCollection coll = sc.Get();
+                        foreach (ManagementObject obj in coll)
+                        {
+                            dataGridView1.Rows.Add(obj["ProcessId"], obj["Name"], obj["CSName"]);
+                        }
+                    }         
+                }
+                catch (Exception ee)
+                {
+                    MessageBox.Show(ee.Message.ToString(), this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter Server Name please", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+            
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount > 1)
+            {
+                dataGridView1.Rows.Clear();
+                System.Threading.Thread.Sleep(1000);
+                butaction();
+            }
+            else
+            {
+                butaction();
+            }
+        }
+        
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("RightClick the PID and Select Close",this.Text,MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+  
+                foreach (DataGridViewCell item in this.dataGridView1.SelectedCells)
+            {
+                // MessageBox.Show(item.Value.ToString());
+                if (item.OwningColumn.Name.ToString().Contains("PID"))
+                {
+                    MessageBox.Show("Selected PID " + item.Value.ToString() + " will be closed", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    try
+                    {
+                        using (ManagementObjectSearcher sc1 = new ManagementObjectSearcher("\\\\" + textBox1.Text.ToString() + "\\root\\cimv2", "select * from win32_process where ProcessId=" + item.Value))
+                        {
+
+                            foreach (ManagementObject obj1 in sc1.Get())
+                            {
+                                object result = obj1.InvokeMethod("Terminate", null);
+                                switch (Convert.ToInt16(result))
+                                {
+                                    case 0:
+                                        MessageBox.Show("Selected PID " + item.Value.ToString() + "  closed and statuscode " + result, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                                        break;
+                                    default:
+                                        MessageBox.Show("Error and statuscode " + result, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                                        break;
+                                }
+
+                            }
+
+                        }
+                    }
+                    catch(Exception ee)
+                    {
+                        MessageBox.Show(ee.Message.ToString(), this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Only PID should be selected", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+
+                    
+
+
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = 3;
+
+            dataGridView1.Columns[0].Name = "PID";
+            dataGridView1.Columns[1].Name = "ProcessName";
+            dataGridView1.Columns[2].Name = "ServerName";
+        }
+    }
+}
